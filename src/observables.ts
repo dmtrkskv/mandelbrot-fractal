0df426b548e1f8b091c7c63c1bd72ace1e08f936
Your Name <dmtrkskv@gmail.com>
2020-03-21 13:35:14 +0300

    feat: добавлена инициализация размера курсора

--- src/observables.ts
+++ src/observables.ts
@@ -1,6 +1,6 @@
 import { TVec2 } from "./index";
 import { fromEvent, combineLatest } from "rxjs";
-import { scan, map, } from "rxjs/operators";
+import { scan, map, startWith } from "rxjs/operators";
 
 export const getCanvasObservable = (canvas: HTMLCanvasElement) => {
     const wheelDeltaToScale = (delta: number) => delta < 0 ? -(125 / delta) : delta / 125;
@@ -16,10 +16,13 @@ export const getCanvasObservable = (canvas: HTMLCanvasElement) => {
         return [clientX - offsetLeft, clientY - offsetTop] as TVec2;
     }));
 
+    const initialWheel = -250;
+
     const wheelScale$ = mouseWheel$.pipe(
         map(e => (e as MouseWheelEvent).deltaY),
-        scan((acc, delta) => acc + delta, 0),
-        map(wheelDeltaToScale),
+        scan((acc, delta) => acc + delta, initialWheel),
+        map(wheelDeltaToScale),    
+        startWith(wheelDeltaToScale(initialWheel))
     );
 
     return combineLatest(wheelScale$, mouseCenter$);

84fe5606ca5cea63a7f717fa9d67ba410511eee3
Your Name <dmtrkskv@gmail.com>
2020-03-21 15:38:56 +0300

    fix: фикс некорректного отображения курсора при прокурчивании страницы

--- src/observables.ts
+++ src/observables.ts
@@ -1,6 +1,6 @@
 import { TVec2 } from "./index";
 import { fromEvent, combineLatest } from "rxjs";
-import { scan, map, startWith, share, withLatestFrom } from "rxjs/operators";
+import { scan, map, startWith, share, withLatestFrom, tap } from "rxjs/operators";
 
 export const getCanvasObservables = (canvas: HTMLCanvasElement) => {
     const wheelDeltaToScale = (delta: number) => delta < 0 ? -(125 / delta) : delta / 125;
@@ -10,24 +10,28 @@ export const getCanvasObservables = (canvas: HTMLCanvasElement) => {
     const mouseWheel$ = fromEvent(canvas, "mousewheel");
 
     const mouseCenter$ = mouseMove$.pipe(map(e => {
-        const { clientX, clientY } = e as MouseEvent;
-        const {offsetLeft, offsetTop} = canvas;
+        const { pageX: x, pageY: y } = e as MouseEvent;
+        const { offsetLeft, offsetTop } = canvas;
 
-        return [clientX - offsetLeft, clientY - offsetTop] as TVec2;
+        return [x - offsetLeft, y - offsetTop] as TVec2;
     }));
 
     const initialWheelDelta = -250;
 
     const wheelScale$ = mouseWheel$.pipe(
+        tap(e => e.preventDefault()),
         map(e => (e as MouseWheelEvent).deltaY),
         scan((acc, delta) => acc + delta, initialWheelDelta),
-        map(wheelDeltaToScale),    
+        map(wheelDeltaToScale),
         startWith(wheelDeltaToScale(initialWheelDelta))
     );
 
     const sharedSelect$ = combineLatest(wheelScale$, mouseCenter$).pipe(share());
 
-    const confirm$ = mouseDown$.pipe(withLatestFrom(sharedSelect$), map(values => values[1]));
+    const confirm$ = mouseDown$.pipe(
+        withLatestFrom(sharedSelect$),
+        map(values => values[1])
+    );
 
-    return  { confirm$, select$: sharedSelect$ };
+    return { confirm$, select$: sharedSelect$ };
 }
